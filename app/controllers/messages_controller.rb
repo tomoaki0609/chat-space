944b0e43df02e1f1ed9832cade163bbec73475eb
tomoaki0609 <asiasi.doba.dojo@gmail.com>
2017-09-21 15:59:27 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,10 +3,18 @@ class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
     @groups = current_user.groups
+    @message = Message.new
+    @messages = Message.all
   end
 
   def create
-    Message.cteate(user_id: "current_user", group_id: "params[:group_id]",body: "params[:body]")
+    Message.create(message_params)
+    redirect_to group_messages_path
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :user_id).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 
 end
