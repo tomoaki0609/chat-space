73e69f82543471ba6d733d50719804f26301897c
tomoaki0609 <asiasi.doba.dojo@gmail.com>
2017-09-25 17:40:11 +0900

    モデルのテスト

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 describe MessagesController do
   describe 'GET #index' do
-    it "assigns the requested message to @message,@messages,@group,@groups" do
+    it "assigns the requested message to @message" do
+      messages = create_list(:message, 3)
+      get :index
+      expect(assigns(:message)).to match(tweets)
     end
 
     it "renders the :index template" do
-      get
+      get :index
+      expect(response).to render_template :index
     end
   end
 
