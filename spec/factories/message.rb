6c8ad48a51e332afa13b4811212337b8b62ebeb5
tomoaki0609 <asiasi.doba.dojo@gmail.com>
2017-09-26 21:45:46 +0900

    コントローラのテスト

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,11 +1,9 @@
-require 'faker'
-
 FactoryGirl.define do
-
   factory :message do
-    body                  Faker::Name.name
+    body                  Faker::Lorem.word
     image                 Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/きりん.jpeg'))
-    user_id               1
-    group_id              1
+    group_id 1
+    user_id 1
   end
+
 end
